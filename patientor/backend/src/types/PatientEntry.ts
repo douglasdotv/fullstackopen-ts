bb737294efd3e100d2ca44c5fcb626bd90aff1e1
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-11-13 15:57:43 -0300

    Enhance PatientEntry type in the backend (9.23)
    - Added HealthCheckRating enum and defined types for HealthCheckPatientEntry, OccupationalHealthcarePatientEntry and HospitalPatientEntry
    - Updated PatientEntry type to be a union of patient entry types
    - Updated sample patient data

--- patientor/backend/src/types/PatientEntry.ts
+++ patientor/backend/src/types/PatientEntry.ts
@@ -1,2 +1,37 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface PatientEntry {}
+import { Diagnosis } from './Diagnosis';
+import { HealthCheckRating } from './HealthCheckRating';
+
+interface BasePatientEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckPatientEntry extends BasePatientEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcarePatientEntry extends BasePatientEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalPatientEntry extends BasePatientEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type PatientEntry =
+  | HealthCheckPatientEntry
+  | OccupationalHealthcarePatientEntry
+  | HospitalPatientEntry;
